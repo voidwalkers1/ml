1d5a1a6aa3ce74e74311e580fd94d74e449e777a
voidwalkers1 <58391597+voidwalkers1@users.noreply.github.com>
2019-12-04 08:03:17 +0530

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,51 +1,29 @@
-import numpy as np
-import pandas as pd
-
-data=pd.DataFrame(data=pd.read_csv('examples.csv'))
-concepts = np.array(data.iloc[:,0:-1])
-target = np.array(data.iloc[:,-1])
-print(data)
-
-def learn(concepts, target):
-    specific_h = concepts[0].copy()
-    print("\nInitialization of specific_h and general_h")
-    print(specific_h)
-    general_h = [["?" for i in range(len(specific_h))] for i in range(len(specific_h))]
-    print(general_h[0])
-    
-    for i, h in enumerate(concepts):
-        if target[i] == "Yes":
-            for x in range(len(specific_h)):
-                if h[x] != specific_h[x]:
-                    specific_h[x] = '?'
-                    general_h[x][x] = '?'
-
-        if target[i] == "No":
-            for x in range(len(specific_h)):
-                if h[x] != specific_h[x]:
-                    general_h[x][x] = specific_h[x]
-                else:
-                    general_h[x][x] = '?'
-                    
-        print("\nSteps of Candidate Elimination Algorithm",i+1)
-        print(specific_h)
-        indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]
-        
-        if len(indices) == len(specific_h):
-            print(general_h[0])
-        else:
-            del_h = general_h
-            for i in range(len(specific_h)):
-                if i not in indices:
-                    print(del_h[i],end=" ")
-        print()
-    indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]
-    for i in indices:
-        general_h.remove(['?', '?', '?', '?', '?', '?'])
-    
-    return specific_h, general_h
-
-s_final, g_final = learn(concepts, target)
-print("\nFinal Specific_h:", s_final, sep="\n")
-print("Final General_h:", g_final, sep="\n")
-
+import csv
+a=list(csv.reader(open('2.csv','r')))
+print(a)
+s=['$','$','$','$','$','$']
+g=[['?','?','?','?','?','?']]
+for i in range(len(a)):
+    if(a[i][6]=="Y"):
+        for j in range(len(s)):
+            if(s[j]=='$'):
+                s[j]=a[i][j]
+               
+            elif(s[j]!=a[i][j] and s[j]!="?"):
+                for k in range(len(g)):
+                    if g[k][j]==s[k]:
+                        g.pop(k)
+                        break
+                s[j]="?"
+        print("The specific array at {} is".format(i), s)
+        print("The general array at {} is".format(i), g)
+    elif(a[i][6]=="N"):
+        for j in range(len(s)):
+            b=['?','?','?','?','?','?']
+            if g[0]==b:
+                g.pop(0)
+            if( s[j]!=a[i][j]and s[j]!='?'):
+                b[j]=s[j]
+                g.append(b)
+        print("The specific array at {} is".format(i), s)
+        print("The general array at {} is".format(i), g)
